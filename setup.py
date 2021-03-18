d4ec2dafc1492b0c32f0753024a8ac0aded8bab2
wbx010512 <481742959@qq.com>
2021-03-18 11:48:34 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example_packg_",
+    version="0.0.1",
+    author="wbx",
+    author_email="wbx481742959@Outlook.com",
+    description="a example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wbx010512/pythonProject.git",
+    packages=setuptools.find_packages(),
+    install_requires=[''],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+# 下载 git
